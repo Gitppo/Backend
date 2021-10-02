85c5a2462fa75385c09e8beaa811808807798d61
RunaNam <nmj5012@naver.com>
2021-10-03 03:03:55 +0900

    Feat: Add initial app

---
+++ src/main/java/HYlikelion/gitppo/GitppoApplication.java
@@ -0,0 +1,13 @@
+package HYlikelion.gitppo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitppoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitppoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HYlikelion/gitppo/GitppoApplicationTests.java
@@ -0,0 +1,13 @@
+package HYlikelion.gitppo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitppoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
