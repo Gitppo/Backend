93e7e3615fe42ea53e13c957b14d6101710477bf
RunaNam <nmj5012@naver.com>
2021-10-07 02:46:50 +0900

    📰Feat: Add files for Nginx
    AWS ec2 서버에서 빌드할 파일 추가
    
    Related to:#1

--- src/main/java/HYlikelion/gitppo/GitppoApplication.java
+++ src/main/java/HYlikelion/gitppo/GitppoApplication.java
@@ -2,12 +2,21 @@ package HYlikelion.gitppo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class GitppoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitppoApplication.class, args);
-	}
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+        + "classpath:application.yml,"
+        + "classpath:real-application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(GitppoApplication.class)
+            .properties(APPLICATION_LOCATIONS)
+            .run(args);
+    }
 
 }
