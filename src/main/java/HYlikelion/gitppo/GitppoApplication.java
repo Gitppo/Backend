6357dfd1e3b655af1fccefc9687606af20ebbffc
HYUNAHSHIM <hyunahshim@naver.com>
2021-10-11 13:02:29 +0900

    🚀Update real-application.yml file real-application.yml 파일 추가하고 gitignore에도 추가 -> 데이터베이스 정보 git에 노출하지 않기 위해
    Related to: #1

--- src/main/java/HYlikelion/gitppo/GitppoApplication.java
+++ src/main/java/HYlikelion/gitppo/GitppoApplication.java
@@ -9,7 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class GitppoApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-        + "classpath:application.yml";
+        + "classpath:application.yml"
+        + ",classpath:real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GitppoApplication.class)
